ab3b5d784ae5d43f15a99f121b79f5a4c139b03a
ansarali41 <ansar.ict14@gmail.com>
2021-03-31 14:59:03 +0600

    first commit

---
+++ src/components/about.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+import myImage from "../img/myImage.png";
+
+class About extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      skills: [
+        { id: "HTML5_skill", content: "HTML5", porcentage: "80%", value: "80" },
+        { id: "CSS3_skill", content: "CSS3", porcentage: "75%", value: "75" },
+        {
+          id: "JavaScript_skill",
+          content: "JavaScript",
+          porcentage: "90%",
+          value: "90"
+        },
+        {
+          id: "ReactJS_skill",
+          content: "ReactJS",
+          porcentage: "80%",
+          value: "80"
+        },
+        {
+          id: "Python_skill",
+          content: "Python",
+          porcentage: "75%",
+          value: "75"
+        },
+        {
+          id: "C/C++_skill",
+          content: "C/C++",
+          porcentage: "85%",
+          value: "85"
+        }
+      ],
+      about_me: [
+        {
+          id: "first-p-about",
+          content:
+            "Hello!I am Afroza.I am Studying CSE in B.Sc at Daffodil International University."
+        },
+        {
+            id: "second-p-about",
+            content:
+              "I am a developer.I am working with C/C++, java-script, HTML, CSS, Bootstrap,Python, React js.I have interest about something else.I want to be successful full stack developer."
+          },
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <section id="about" className="about-mf sect-pt4 route">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="box-shadow-full">
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="row">
+                      <div
+                        className="col-sm-6 col-md-5"
+                        style={{ margin: "0 auto" }}
+                      >
+                        <div
+                          className="about-img"
+                          style={{ textAlign: "center" }}
+                        >
+                          <img
+                            className="img-fluid rounded b-shadow-a"
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="skill-mf">
+                      {/* <p className="title-s">Skill</p> */}
+                      {this.state.skills.map(skill => {
+                        return (
+                          <React.Fragment key={skill.id}>
+                            <span>{skill.content}</span>{" "}
+                            <span className="pull-right">
+                              {skill.porcentage}
+                            </span>
+                            <div className="progress">
+                              <div
+                                className="progress-bar"
+                                role="progressbar"
+                                style={{ width: skill.porcentage }}
+                                aria-valuenow={skill.value}
+                                aria-valuemin="0"
+                                aria-valuemax="100"
+                              ></div>
+                            </div>
+                          </React.Fragment>
+                        );
+                      })}
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="about-me pt-4 pt-md-0">
+                      <div className="title-box-2">
+                        <h5 className="title-left">About Me</h5>
+                      </div>
+                      {this.state.about_me.map(content => {
+                        return (
+                          <p className="lead" key={content.id}>
+                            {content.content}
+                          </p>
+                        );
+                      })}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default About;
